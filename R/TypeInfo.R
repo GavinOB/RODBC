b58bc305bc9c3912b7f70cc1346e0b61d71a65c9
Gavin O'Brien <geobrien@gmail.com>
2016-09-01 20:01:32 -0400

    added SQLite Date type

--- R/TypeInfo.R
+++ R/TypeInfo.R
@@ -53,7 +53,7 @@ typesR2DBMS <-
                   Oracle = list(double="binary_double", integer="decimal",
                   character="varchar(255)", logical="varchar(5)"),
                   SQLite = list(double="double", integer="integer",
-                  character="varchar(255)", logical="varchar(5)"),
+                  character="varchar(255)", logical="varchar(5)", date="date"),
                   EXCEL = list(double="NUMBER", integer="NUMBER",
                   character="VARCHAR(255)", logical="LOGICAL"),
                   DBASE = list(double="Numeric", integer="Numeric",
