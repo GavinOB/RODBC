ccc5ae2795186bf734b594613c489a328ee39c8d
Gavin O'Brien <gobrien@servings.org>
2016-09-09 12:25:40 -0400

    back to logical

--- R/TypeInfo.R
+++ R/TypeInfo.R
@@ -43,7 +43,7 @@ typesR2DBMS <-
                   MySQL = list(double="double", integer="integer",
                   character="varchar(255)", logical="varchar(5)", date="double"),
                   ACCESS = list(double="DOUBLE", integer="INTEGER",
-                  character="VARCHAR(255)", logical="SHORT", date="DATE"),
+                  character="VARCHAR(255)", logical="LOGICAL", date="DATE"),
                   ## float is double, real is single
                   "Microsoft SQL Server" = list(double="float", integer="int",
                   character="varchar(255)", logical="varchar(5)", date="float"),
